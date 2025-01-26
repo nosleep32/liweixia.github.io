ef6e5e409e06761c2c6ae9e5f7e4696f5f6d5e24
nosleep32 <144634793+nosleep32@users.noreply.github.com>
2025-01-26 13:21:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
+    // Sidebar toggling
+    var menuIcon = document.getElementById('menu-icon');
+    var closeBtn = document.getElementById('close-btn');
+    var sidenav = document.getElementById('mySidenav');
+
+    menuIcon.addEventListener('click', function() {
+        // Expand the sidebar
+        sidenav.style.width = '250px';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        // Collapse the sidebar
+        sidenav.style.width = '0';
+    });
+
+    // Fade-out effect for links
     var links = document.querySelectorAll(".sidenav a");
 
     links.forEach(function(link) {
@@ -13,20 +29,5 @@ document.addEventListener("DOMContentLoaded", function() {
                 window.location.href = targetPage;
             }, 500); // Duration of the fade-out effect
         });
-    });
-});
-
-// Sidebar toggling
-document.addEventListener("DOMContentLoaded", function() {
-    var menuIcon = document.getElementById('menu-icon');
-    var closeBtn = document.getElementById('close-btn');
-    var sidenav = document.getElementById('mySidenav');
-
-    menuIcon.addEventListener('click', function() {
-        sidenav.style.width = '250px';
-    });
-
-    closeBtn.addEventListener('click', function() {
-        sidenav.style.width = '0';
     });
 });
